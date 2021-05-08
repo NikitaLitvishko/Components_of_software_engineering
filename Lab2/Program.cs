e5c2574c8fb806aeb8a8a394d14a3ab65fbf7a1f
NikitaLitvishko <55049232+NikitaLitvishko@users.noreply.github.com>
2021-05-08 13:45:38 +0300

    Add files via upload

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -5,7 +5,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        MultipleBinaryFlag flag = new MultipleBinaryFlag(1, true);
-        Console.WriteLine(flag.GetFlag());
+        MultipleBinaryFlag flag = new MultipleBinaryFlag(2, false);
+
+        bool? exp = flag.GetFlag();
+
+        Console.WriteLine(exp);
+
+        flag.ResetFlag(0);
+        flag.ResetFlag(1);
+
+        bool? act = flag.GetFlag();
+
+        Console.WriteLine(act);
     }
 }
--- Lab2/TestClass.cs
+++ Lab2/TestClass.cs
@@ -4,6 +4,8 @@ using System;
 
 public class TestClass
 {
+    // Constructor Tests
+
     [Fact]
     public void Constructor_zeroTrue_Exception()
     {
@@ -26,5 +28,279 @@ public class TestClass
     public void Constructor_oneFalse_Exception()
     {
         Assert.Throws<ArgumentOutOfRangeException>(() => new MultipleBinaryFlag(1, false));
+    }
+
+    [Fact]
+    public void Constructor_moreThanOneTrue_NotNull()
+    {
+        MultipleBinaryFlag flag =  new MultipleBinaryFlag(4, true);
+
+        Assert.NotNull(flag);
+    }
+
+    [Fact]
+    public void Constructor_moreThanOneFalse_NotNull()
+    {
+        MultipleBinaryFlag flag =  new MultipleBinaryFlag(4, false);
+
+        Assert.NotNull(flag);
+    }
+
+    [Fact]
+    public void Constructor_moreThanOneNothing_NotNull()
+    {
+        MultipleBinaryFlag flag =  new MultipleBinaryFlag(4);
+
+        Assert.NotNull(flag);
+    }
+
+    [Fact]
+    public void Constructor_maxUlongNothing_Exception()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new MultipleBinaryFlag(ulong.MaxValue));
+    }
+
+    [Fact]
+    public void Constructor_maxUlongTrue_Exception()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new MultipleBinaryFlag(ulong.MaxValue, true));
+    }
+
+    [Fact]
+    public void Constructor_maxUlongFalse_Exception()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new MultipleBinaryFlag(ulong.MaxValue, false));
+    }
+
+    [Fact]
+    public void Constructor_moreThanOneFalse_True()
+    {
+        MultipleBinaryFlag flag = new MultipleBinaryFlag(2, true);
+
+        Assert.True(flag.GetFlag());
+    }
+
+    [Fact]
+    public void Constructor_moreThanOneFalse_False()
+    {
+        MultipleBinaryFlag flag = new MultipleBinaryFlag(2, false);
+
+        Assert.False(flag.GetFlag());
+    }
+
+    [Fact]
+    public void Constructor_moreThanOneNothing_True()
+    {
+        MultipleBinaryFlag flag = new MultipleBinaryFlag(2);
+
+        Assert.True(flag.GetFlag());
+    }
+
+    // Dispose Tests
+
+    [Fact]
+    public void Dispose_Nothing_Null()
+    {
+        MultipleBinaryFlag flag = new MultipleBinaryFlag(2, false);
+
+        flag.Dispose();
+
+        Assert.Null(flag.GetFlag());
+    }
+
+    [Fact]
+    public void Dispose_Nothing_NotEqual()
+    {
+        MultipleBinaryFlag flag = new MultipleBinaryFlag(2, false);
+        bool? exp = flag.GetFlag();
+
+        flag.Dispose();
+
+        bool? act = flag.GetFlag();
+
+        Assert.NotEqual(exp, act);
+    }
+
+    // GetFlag Tests
+
+    [Fact]
+    public void GetFlag_Nothing_TrueEqual()
+    {
+        MultipleBinaryFlag flag = new MultipleBinaryFlag(5, true);
+
+        bool? exp = true;
+
+        bool? act = flag.GetFlag();
+
+        Assert.Equal(exp, act);
+    }
+
+    [Fact]
+    public void GetFlag_Nothing_FalseEqual()
+    {
+        MultipleBinaryFlag flag = new MultipleBinaryFlag(5, false);
+
+        bool? exp = false;
+
+        bool? act = flag.GetFlag();
+
+        Assert.Equal(exp, act);
+    }
+
+    [Fact]
+    public void GetFlag_Nothing_TrueNotEqual()
+    {
+        MultipleBinaryFlag flag = new MultipleBinaryFlag(5, true);
+
+        bool? exp = false;
+
+        bool? act = flag.GetFlag();
+
+        Assert.NotEqual(exp, act);
+    }
+
+    [Fact]
+    public void GetFlag_Nothing_FalseNotEqual()
+    {
+        MultipleBinaryFlag flag = new MultipleBinaryFlag(5, false);
+
+        bool? exp = true;
+
+        bool? act = flag.GetFlag();
+
+        Assert.NotEqual(exp, act);
+    }
+
+    // GetFlag / SetFlag Tests
+
+    [Fact]
+    public void GetFlag_SetFlag_FalseEqual()
+    {
+        MultipleBinaryFlag flag = new MultipleBinaryFlag(2, false);
+
+        bool? exp = flag.GetFlag();
+
+        flag.SetFlag(0);
+
+        bool? act = flag.GetFlag();
+
+        Assert.Equal(exp, act);
+    }
+
+    [Fact]
+    public void GetFlag_SetFlag_FalseNotEqual()
+    {
+        MultipleBinaryFlag flag = new MultipleBinaryFlag(2, false);
+
+        bool? exp = flag.GetFlag();
+
+        flag.SetFlag(0);
+        flag.SetFlag(1);
+
+        bool? act = flag.GetFlag();
+
+        Assert.NotEqual(exp, act);
+    }
+
+    [Fact]
+    public void GetFlag_SetFlag_TrueEqual()
+    {
+        MultipleBinaryFlag flag = new MultipleBinaryFlag(2, true);
+
+        bool? exp = flag.GetFlag();
+
+        flag.SetFlag(0);
+
+        bool? act = flag.GetFlag();
+
+        Assert.Equal(exp, act);
+    }
+
+    [Fact]
+    public void GetFlag_SetFlag_NothingEqual()
+    {
+        MultipleBinaryFlag flag = new MultipleBinaryFlag(2);
+
+        bool? exp = flag.GetFlag();
+
+        flag.SetFlag(0);
+
+        bool? act = flag.GetFlag();
+
+        Assert.Equal(exp, act);
+    }
+
+    // GetFlag / ResetFlag Tests
+
+    [Fact]
+    public void GetFlag_ResetFlag_FalseEqual()
+    {
+        MultipleBinaryFlag flag = new MultipleBinaryFlag(2, false);
+
+        bool? exp = flag.GetFlag();
+
+        flag.ResetFlag(0);
+
+        bool? act = flag.GetFlag();
+
+        Assert.Equal(exp, act);
+    }
+
+    [Fact]
+    public void GetFlag_ResetFlag_TrueNotEqual()
+    {
+        MultipleBinaryFlag flag = new MultipleBinaryFlag(2, true);
+
+        bool? exp = flag.GetFlag();
+
+        flag.ResetFlag(0);
+
+        bool? act = flag.GetFlag();
+
+        Assert.NotEqual(exp, act);
+    }
+
+    [Fact]
+    public void GetFlag_ResetFlag_TrueEqual()
+    {
+        MultipleBinaryFlag flag = new MultipleBinaryFlag(2, true);
+
+        bool? exp = flag.GetFlag();
+
+        flag.ResetFlag(0);
+        flag.ResetFlag(1);
+
+        bool? act = flag.GetFlag();
+
+        Assert.NotEqual(exp, act);
+    }
+
+    [Fact]
+    public void GetFlag_ResetFlag_NothingNotEqual()
+    {
+        MultipleBinaryFlag flag = new MultipleBinaryFlag(2);
+
+        bool? exp = flag.GetFlag();
+
+        flag.ResetFlag(0);
+
+        bool? act = flag.GetFlag();
+
+        Assert.NotEqual(exp, act);
+    }
+
+    [Fact]
+    public void GetFlag_ResetFlag_NothingEqual()
+    {
+        MultipleBinaryFlag flag = new MultipleBinaryFlag(2);
+
+        bool? exp = flag.GetFlag();
+
+        flag.ResetFlag(0);
+        flag.ResetFlag(1);
+
+        bool? act = flag.GetFlag();
+
+        Assert.NotEqual(exp, act);
     }
 }
