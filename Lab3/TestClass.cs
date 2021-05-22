c21a8ddb44a71da10953a474cdcc11c88d107b06
NikitaLitvishko <55049232+NikitaLitvishko@users.noreply.github.com>
2021-05-22 23:32:18 +0300

    Add files via upload

--- Lab3/TestClass.cs
+++ Lab3/TestClass.cs
@@ -4,5 +4,89 @@ using System;
 
 public class TestClass
 {
+    public void ResetPasswordHasher()
+    {
+        PasswordHasher.Init("put your soul(or salt) here", 65521);
+    }
 
+    // 0_2_3_4_5
+
+    [Fact]
+    public void ExecutionRoute_0_2_3_4_5_EverythingIsOK_CheckSameValues()
+    {
+        string pass = "passwordForLab3";
+        string exp = PasswordHasher.GetHash(pass);
+
+        Assert.Equal(exp, PasswordHasher.GetHash(pass));
+
+        ResetPasswordHasher();
+    }
+
+    [Fact]
+    public void ExecutionRoute_0_2_3_4_5_EverythingIsOK()
+    {
+        try
+        {
+            PasswordHasher.Init("Salt_number_1", 1);
+
+            Assert.True(true);
+        }
+        catch (Exception)
+        {
+            Assert.False(true);
+        }
+        finally
+        {
+            ResetPasswordHasher();
+        }
+    }
+
+    [Fact]
+    public void ExecutionRoute_0_2_3_4_5_EverythingIsOK_CheckDifferentValues()
+    {
+        string pass = "passwordForLab3";
+        string exp = PasswordHasher.GetHash(pass);
+
+        PasswordHasher.Init("Salt_number_1", 1);
+
+        Assert.NotEqual(exp, PasswordHasher.GetHash(pass));
+
+        ResetPasswordHasher();
+    }
+
+    // 0_1_2_3_4_5
+
+    [Fact]
+    public void ExecutionRoute_0_1_2_3_4_5_CatchException_CheckDifferentValues()
+    {
+        string pass = "لْعَرَبِيَّةُ";
+        string exp = PasswordHasher.GetHash(pass);
+
+        PasswordHasher.Init("Salt_number_1", 1);
+
+        Assert.NotEqual(exp, PasswordHasher.GetHash(pass));
+
+        ResetPasswordHasher();
+    }
+
+    // 0_3_5
+
+    [Fact]
+    public void ExecutionRoute_0_3_5_AllSkipped_CheckSameValues()
+    {
+        string pass = "passwordForLab3";
+        string exp = PasswordHasher.GetHash(pass);
+
+        PasswordHasher.Init("", 0);
+
+        Assert.Equal(exp, PasswordHasher.GetHash(pass));
+
+        PasswordHasher.Init(null, 0);
+
+        Assert.Equal(exp, PasswordHasher.GetHash(pass));
+
+        ResetPasswordHasher();
+    }
+
+    // 
 }
